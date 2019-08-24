0c5852783d82cea4363254c9a1ad4c94885b9111
Derevyanchenko <40884135+Derevyanchenko@users.noreply.github.com>
2019-08-24 21:10:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  // steps
+
+  function showNextStep(el) {
+    let curr_step = el.closest(".step").data("step"),
+    next_step = curr_step+1;
+
+    $(".step").hide();
+    $(".step[data-step='" + parseInt(next_step) + "']").show();
+  }
+
+  // show first step
+
+  $(".step-1").show();
+
+  $(".radio").on("change", function(that) {
+    that = $(this);  
+    showNextStep(that);
+  });
+
+  
+  $(".js-btn_next").on("click", function(that) {
+    that.preventDefault();
+    that = $(this);
+
+      showNextStep(that);
+  });
+
+
+});
